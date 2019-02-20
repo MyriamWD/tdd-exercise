7a19cb96d884c8717c0f0d6ed3902e2848dd750d
MyriamWD <myriam.waldend@gmail.com>
2019-02-20 13:58:58 -0800

    Added blackjack score test

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,25 @@
 # blackjack_score.rb
+require "awesome_print"
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, "King", "Queen", "Jack"]
+hand = []
+hand_random = rand(2...5)
+
+hand_random.times do
+  random_card = rand(0...VALID_CARDS.length)
+  hand << VALID_CARDS[random_card]
+end
 
 def blackjack_score(hand)
-
+  hand_numeric = hand.map do |card|
+    card.class == String ? 10 : card
+  end
+  # coding for ace as 11
+  #   ace = hand_numeric.map do |card|
+  #     card = 1?
+  #   end
+  score = hand_numeric.reduce(:+)
+  return score
 end
+
+puts blackjack_score(hand)
--- specs/blackjack_score_spec.rb
+++ specs/blackjack_score_spec.rb
@@ -1,15 +1,15 @@
-require 'minitest'
-require 'minitest/spec'
-require 'minitest/autorun'
-require 'minitest/reporters'
-require 'minitest/pride'
+require "minitest"
+require "minitest/spec"
+require "minitest/autorun"
+require "minitest/reporters"
+require "minitest/pride"
 
-require_relative '../lib/blackjack_score'
+require_relative "../lib/blackjack_score"
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-describe 'Blackjac Score' do
-  it 'can calculate the score for a pair of number cards' do
+describe "Blackjac Score" do
+  it "can calculate the score for a pair of number cards" do
 
     # Arrange
     hand = [3, 4]
@@ -18,26 +18,41 @@ describe 'Blackjac Score' do
     score = blackjack_score(hand)
 
     # Assert <-  You do this part!
+    expect(score).must_equal 7
+  end
+
+  it "facecards have values calculated correctly" do
+    # Arrange
+    facecards = ["King", "Jack", "Queen"]
+    # Act
+    facecards.each do |facecard|
+      score = blackjack_score([facecard, 6])
+      # Assert
+      expect(score).must_equal 16
+    end
+  end
+
+  it "calculates aces as 11 where it does not go over 21" do
+    # Arrange
+    hand = [1, 1]
+    # Act
+    score = blackjack_score(hand)
+    # Assert
 
   end
 
-  it 'facecards have values calculated correctly' do
+  it "calculates aces as 1, if an 11 would cause the score to go over 21" do
+    (2..10).each do |card|
+      # Arrange
+      hand = [card, 1]
+      # Act
 
+    end
   end
 
-  it 'calculates aces as 11 where it does not go over 21' do
-
+  it "raises an ArgumentError for invalid cards" do
   end
 
-  it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
-
-  end
-
-  it 'raises an ArgumentError for invalid cards' do
-
-  end
-
-  it 'raises an ArgumentError for more than 5 cards in the hand' do
-
+  it "raises an ArgumentError for more than 5 cards in the hand" do
   end
 end
